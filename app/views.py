de83efe9ed5f752aa050de757986b40eb0681cff
Sh0ma28 <igordomanskii0896@gmail.com>
2024-09-02 06:41:36 +1000

    styles

--- app/views.py
+++ app/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 import requests
 
-MEDIA_TYPES = ['image', 'document']
-
 # Create your views here.
 def index(request):
     path = ''
@@ -15,17 +13,7 @@ def index(request):
             path = request.POST.get('path')
         r = requests.get(f'https://cloud-api.yandex.net/v1/disk/public/resources?public_key={key}&limit=100&path={path}')
         items = r.json().get('_embedded', {}).get('items', [])
-        data = {'dir':[], 'other':[]}
-        for type in MEDIA_TYPES:
-            data[type] = []
-        for item in items:
-            if item['type'] == 'dir':
-                data['dir'].append(item)
-            elif item['media_type'] in MEDIA_TYPES:
-                data[item['media_type']].append(item)
-            else:
-                data['other'].append(item)
-        return render(request, 'app/index.html', {'data': data,
+        return render(request, 'app/index.html', {'data': items,
                                                   'name': r.json().get('name'),
                                                   'public_key': key,
                                                   'path': path})
